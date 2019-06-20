a4907a180399f9fafb90257bede8451b9eb8db5f
Adhamsheriff <adhamsheriff.a@oclocksoftware.com>
2019-06-20 20:33:43 +0530

    First Time

---
+++ app/src/main/java/com/adhamsheriff/firestore/LoginActivity.java
@@ -0,0 +1,50 @@
+package com.adhamsheriff.firestore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    FirebaseFirestore db;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        db = FirebaseFirestore.getInstance();
+
+        findViewById(R.id.button_check_login).setOnClickListener(this);
+
+    }
+
+    public void checkLogin(){
+        db.collection("login")
+                .whereEqualTo("mobile","9791516629")
+                .whereEqualTo("password","1234563")
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        if (!queryDocumentSnapshots.isEmpty()) {
+                            Toast.makeText(getApplicationContext(),"Hello...",Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.button_check_login:
+                checkLogin();
+                break;
+        }
+    }
+}
