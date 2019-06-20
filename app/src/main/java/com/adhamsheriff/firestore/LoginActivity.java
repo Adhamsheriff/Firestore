242b234bf6ed4dc8cc576a74a469eb78c451e423
Adhamsheriff <adhamsheriff.a@oclocksoftware.com>
2019-06-20 21:42:26 +0530

    Login check

--- app/src/main/java/com/adhamsheriff/firestore/LoginActivity.java
+++ app/src/main/java/com/adhamsheriff/firestore/LoginActivity.java
@@ -1,10 +1,13 @@
 package com.adhamsheriff.firestore;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -12,6 +15,7 @@ import com.google.firebase.firestore.QuerySnapshot;
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     FirebaseFirestore db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,25 +27,38 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     }
 
-    public void checkLogin(){
+    public void checkLogin() {
         db.collection("login")
-                .whereEqualTo("mobile","9791516629")
-                .whereEqualTo("password","1234563")
+                .whereEqualTo("mobile", "9791516629")
+                .whereEqualTo("password", "123456")
                 .get()
                 .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                     @Override
                     public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
                         if (!queryDocumentSnapshots.isEmpty()) {
-                            Toast.makeText(getApplicationContext(),"Hello...",Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                        else
+                        {
+                            Toast.makeText(getApplicationContext(), "Invalid Login...", Toast.LENGTH_SHORT).show();
 
                         }
                     }
-                });
+                })
+        .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 
     @Override
     public void onClick(View v) {
-        switch(v.getId()) {
+        switch (v.getId()) {
             case R.id.button_check_login:
                 checkLogin();
                 break;
